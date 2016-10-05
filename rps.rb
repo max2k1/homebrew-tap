2b6b236556a3fbb73d69a6cfb53be57097fce2af
Maksim Kupriianov <maxk@yandex-team.ru>
2016-10-05 14:57:24 +0300

    RPS: 0.0-10

--- rps.rb
+++ rps.rb
@@ -3,9 +3,9 @@ require "language/go"
 class Rps < Formula
   desc "Measure rows-per-second rate by reading access.logs through pipe"
   homepage "https://github.com/max2k1/rps"
-  url "https://github.com/max2k1/rps/archive/0.0-9.tar.gz"
-  version "0.0-9"
-  sha256 "634011b9639fe4a3c0cb6102f01b7b36cbd9dea366677859f5b50f5b33dd3e27"
+  url "https://github.com/max2k1/rps/archive/0.0-10.tar.gz"
+  version "0.0-10"
+  sha256 "b099400af5503d044d06f966fc779d2e9eb80d20b5f5f0a475f4feea100f3e36"
 
   head "https://github.com/max2k1/rps.git"
 
